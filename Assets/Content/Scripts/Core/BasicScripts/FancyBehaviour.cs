c963065826742d740422b84f4e3084e0c68f7b3b
_kote <logkedr18@gmail.com>
2026-06-23 17:21:31 +0400

    add: Turn fancy behaviour parent to network behaviour

--- Assets/Content/Scripts/Core/BasicScripts/FancyBehaviour.cs
+++ Assets/Content/Scripts/Core/BasicScripts/FancyBehaviour.cs
@@ -1,9 +1,10 @@
 using System.Collections.Generic;
+using Mirror;
 using UnityEngine;
 using UnityEngine.AddressableAssets;
 using Zenject;
 
-public abstract class FancyBehaviour : MonoBehaviour
+public abstract class FancyBehaviour : NetworkBehaviour
 {
     [Inject]
     private IEventSystem _eventSys;
